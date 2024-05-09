2e0743b594c7e14f1d38b7c12cf65772cba5618e
Rikard Dotzler <rikardnoer@gmail.com>
2024-05-09 20:37:54 +0200

    enable cors

--- DenoServer/server.ts
+++ DenoServer/server.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak@v16.0.0/mod.ts";
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import { registerPlayerRoutes } from "./player/player.routes.ts";
 import { registerGameRoutes } from "./game/game.routes.ts";
 import { registerEloRoutes } from "./elo/elo.routes.ts";
@@ -16,6 +17,7 @@ registerEloRoutes(api);
 api.get("/", (context) => context.response.redirect("/players"));
 
 app.use(api.routes());
+app.use(oakCors());
 app.use(api.allowedMethods());
 
 await app.listen({ port: 8000 });
