8f288e79d39c4285b7d0df86efa50803ec3f37ea
Rikard Dotzler <rikardnoer@gmail.com>
2024-05-14 21:21:31 +0200

    admin page protector

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { TablePage } from "./components/table-page";
 import { queryClient } from "./common/query-client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { PlayerPage } from "./components/player-page";
-import { AdminPage } from "./components/admin-page";
+import { AdminPageProtector } from "./components/admin-page-protector";
 
 function App() {
   const queryClienta = queryClient;
@@ -15,7 +15,7 @@ function App() {
             <Route path="/" element={<Navigate to="/tennis-table" />} />
             <Route path="/tennis-table" element={<TablePage />} />
             <Route path="/player/:name" element={<PlayerPage />} />
-            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/admin" element={<AdminPageProtector />} />
           </Routes>
         </BrowserRouter>
       </div>
