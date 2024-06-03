1ba2db831748da8d2ec384cc97f4ca2823d0c2ee
Rikard Dotzler <rikardnoer@gmail.com>
2024-06-03 20:59:29 +0200

    graph

--- frontend/src/components/compare-players-page.tsx
+++ frontend/src/components/compare-players-page.tsx
@@ -2,6 +2,21 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { classNames } from "../common/class-names";
 import { useQuery } from "@tanstack/react-query";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ReferenceLine,
+  Tooltip,
+  TooltipProps,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { useWindowSize } from "usehooks-ts";
+import {
+  NameType,
+  ValueType,
+} from "recharts/types/component/DefaultTooltipContent";
 
 type PlayerComparison = {
   allPlayers: string[];
@@ -28,6 +43,8 @@ function usePlayerSummaryQuery(players?: string[]) {
 export const ComparePlayersPage: React.FC = () => {
   const [selectedPlayers, setSelectedPlayers] = useState<string[]>([]);
   const comparison = usePlayerSummaryQuery(selectedPlayers);
+  const { width = 0 } = useWindowSize();
+
   return (
     <div className="flex flex-col items-center">
       <Link
@@ -36,17 +53,60 @@ export const ComparePlayersPage: React.FC = () => {
       >
         Back to leaderboard
       </Link>
-      <section className="space-y-1 my-4">
-        Comparing players
+      <section className="flex flex-col lg:flex-row">
         <PlayerSelector
           players={comparison.data?.allPlayers}
           isLoading={comparison.isLoading}
           selectedPlayers={selectedPlayers}
           setSelectedPlayers={setSelectedPlayers}
         />
-        {comparison.data?.graphData?.map((entry) => (
-          <pre>{JSON.stringify(entry)}</pre>
-        ))}
+        {comparison.data?.graphData && (
+          <LineChart
+            className="mt-7"
+            width={Math.min(730, width)}
+            height={400}
+            data={comparison.data.graphData}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="1 4" vertical={false} />
+            <XAxis
+              dataKey="name"
+              // ?
+            />
+            <YAxis
+              type="number"
+              domain={["dataMin", "dataMax"]}
+              tickFormatter={(value) =>
+                value.toLocaleString("no-NO", {
+                  maximumFractionDigits: 0,
+                })
+              }
+            />
+            <Tooltip
+              formatter={(value) => [
+                value.toLocaleString("no-NO", {
+                  maximumFractionDigits: 0,
+                }),
+                "Elo",
+              ]}
+              wrapperClassName="rounded-lg"
+              animationDuration={0}
+              content={<CustomTooltip />}
+            />
+            {comparison.data.graphData[0] &&
+              Object.keys(comparison.data.graphData[0]).map((player) => (
+                <Line
+                  key={player}
+                  type="monotone"
+                  dataKey={player}
+                  stroke="#8884d8"
+                  animationDuration={100}
+                  dot={false}
+                />
+              ))}
+            <ReferenceLine y={1000} stroke="white" label="1 000" />
+          </LineChart>
+        )}
       </section>
     </div>
   );
@@ -79,7 +139,8 @@ const PlayerSelector: React.FC<{
   const allIsSelected = selectedPlayers.length === storedPlayers.length;
 
   return (
-    <div className="grid grid-cols-1 gap-1 grid-flow-row w-40">
+    <div className="grid grid-cols-1 gap-1 grid-flow-row w-40 h-fit">
+      Comparing players
       <button
         className={classNames(
           "h-8 text-left pl-4 rounded-lg",
@@ -124,4 +185,28 @@ const PlayerSelector: React.FC<{
       })}
     </div>
   );
+};
+
+const CustomTooltip: React.FC = ({
+  active,
+  payload,
+  label,
+}: TooltipProps<ValueType, NameType>) => {
+  if (active && payload && payload.length) {
+    const record = payload[0].payload as Record<string, number>;
+    const entries = Object.entries(record);
+    entries.sort((a, b) => b[1] - a[1]);
+
+    return (
+      <div className="p-2 bg-slate-700 ring-1 ring-white rounded-lg">
+        {entries.map((entry) => (
+          <p key={entry[0]}>{`${entry[0]}: ${entry[1].toLocaleString("no-NO", {
+            maximumFractionDigits: 0,
+          })}`}</p>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
 };
