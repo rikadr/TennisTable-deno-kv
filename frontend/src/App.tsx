20942b146c688ccbc3e097cd17ba2c3924e86130
Peder Fosse <peder.fosse@gmail.com>
2024-06-06 12:04:00 +0200

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./common/query-client";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { PlayerPage } from "./components/player-page";
-import { AdminPageProtector } from "./components/admin-page-protector";
 import { AddPlayerPage } from "./components/add-player-page";
 import { LeaderBoardPage } from "./components/leader-board-page";
 import { AddGamePage } from "./components/add-game-page";
 import { ComparePlayersPage } from "./components/compare-players-page";
 import { MyPage } from "./components/my-page";
 import { LoginPage } from "./components/login";
+import { AdminPage } from "./components/admin-page";
 
 function App() {
   const queryClienta = queryClient;
@@ -18,13 +18,16 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Navigate to="/leader-board" />} />
-            <Route path="/tennis-table" element={<Navigate to="/leader-board" />} />
+            <Route
+              path="/tennis-table"
+              element={<Navigate to="/leader-board" />}
+            />
             <Route path="/leader-board" element={<LeaderBoardPage />} />
             <Route path="/compare-players" element={<ComparePlayersPage />} />
             <Route path="/player/:name" element={<PlayerPage />} />
             <Route path="/add-player" element={<AddPlayerPage />} />
             <Route path="/add-game" element={<AddGamePage />} />
-            <Route path="/admin" element={<AdminPageProtector />} />
+            <Route path="/admin" element={<AdminPage />} />
             <Route path="/me" element={<MyPage />} />
             <Route path="/login" element={<LoginPage />} />
           </Routes>
