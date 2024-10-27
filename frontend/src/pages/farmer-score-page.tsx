281f57111db4f08b57a385f262d9bd93e287f194
Rikard Dotzler <rikardnoer@gmail.com>
2024-10-27 18:11:05 +0100

    update farmer explanation example

--- frontend/src/pages/farmer-score-page.tsx
+++ frontend/src/pages/farmer-score-page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ALLOWED_FARMER_GAMES, FARMER_DIFF_THRESHOLD, FARMER_GAME_LIMIT } from "../wrappers/leaderboard";
 
 export const FarmerScorePage: React.FC = () => {
-  const exampleFarmerGames = 5;
+  const [exampleFarmerGames, setExampleFarmerGames] = React.useState(5);
 
   return (
     <div className="flex flex-col items-center">
@@ -18,9 +18,26 @@ export const FarmerScorePage: React.FC = () => {
         <p>You are allowed {ALLOWED_FARMER_GAMES} farming games before they are counted towards your score.</p>
         <br />
         <h2>What counts as a farming game?</h2>
-        <p>A game you win where the oponent has at least {FARMER_DIFF_THRESHOLD} points lower score.</p>
+        <p>A game you win where the oponent has at least {FARMER_DIFF_THRESHOLD} points lower Elo score than you.</p>
         <br />
         <h2>Example</h2>
+        <div className="flex gap-2">
+          <button
+            disabled={exampleFarmerGames >= FARMER_GAME_LIMIT}
+            className="px-4 py-1 rounded-md bg-blue-500 hover:bg-blue-700"
+            onClick={() => setExampleFarmerGames(exampleFarmerGames + 1)}
+          >
+            +
+          </button>
+          <button
+            disabled={exampleFarmerGames <= 0}
+            className="px-4 py-1 rounded-md bg-blue-500 hover:bg-blue-700"
+            onClick={() => setExampleFarmerGames(exampleFarmerGames - 1)}
+          >
+            -
+          </button>
+        </div>
+        <br />
         <p>
           {exampleFarmerGames} farming games in your last {FARMER_GAME_LIMIT} games
         </p>
@@ -36,9 +53,15 @@ export const FarmerScorePage: React.FC = () => {
           {(exampleFarmerGames - ALLOWED_FARMER_GAMES) / (FARMER_GAME_LIMIT - ALLOWED_FARMER_GAMES)} * 10 ={" "}
           {((exampleFarmerGames - ALLOWED_FARMER_GAMES) / (FARMER_GAME_LIMIT - ALLOWED_FARMER_GAMES)) * 10}
         </p>
-        <p>
+        <p className="text-base font-bold">
           Farmer score is:{" "}
-          <b> {((exampleFarmerGames - ALLOWED_FARMER_GAMES) / (FARMER_GAME_LIMIT - ALLOWED_FARMER_GAMES)) * 10}</b>{" "}
+          <b>
+            {" "}
+            {Math.max(
+              ((exampleFarmerGames - ALLOWED_FARMER_GAMES) / (FARMER_GAME_LIMIT - ALLOWED_FARMER_GAMES)) * 10,
+              0,
+            )}
+          </b>{" "}
         </p>
       </section>
     </div>
