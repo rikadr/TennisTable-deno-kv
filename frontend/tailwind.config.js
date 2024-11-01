a2606138003d7ea3b17f548cadb53b1b240b8c59
Rikard Dotzler <rikardnoer@gmail.com>
2024-11-01 09:06:49 +0100

    Default theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,23 +10,23 @@ module.exports = {
     extend: {
       colors: {
         // Halloween theme
-        primary: {
-          text: "#FFFFFF",
-          background: "#1B0E01",
-        },
-        secondary: {
-          text: "#000000",
-          background: "#d97706",
-        },
-        // Default theme
         // primary: {
         //   text: "#FFFFFF",
-        //   background: "#1e293b",
+        //   background: "#1B0E01",
         // },
         // secondary: {
-        //   text: "#FFFFFF",
-        //   background: "#6b7280",
+        //   text: "#000000",
+        //   background: "#d97706",
         // },
+        // Default theme
+        primary: {
+          text: "#FFFFFF",
+          background: "#1e293b",
+        },
+        secondary: {
+          text: "#FFFFFF",
+          background: "#6b7280",
+        },
       },
     },
   },
