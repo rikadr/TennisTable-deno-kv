a2a69a2b8767b393958e239a36bc07cd91bc3260
Rikard Dotzler <rikardnoer@gmail.com>
2024-11-01 21:50:00 +0100

    update game delete and db import endpoints

--- DenoServer/game/game.routes.ts
+++ DenoServer/game/game.routes.ts
@@ -9,8 +9,9 @@ import {
   getGame,
   importGame,
 } from "./game.ts";
-import { isAuthenticated, requireAuth } from "../auth-service/middleware.ts";
+import { isAuthenticated } from "../auth-service/middleware.ts";
 import { WebSocketClientManager } from "../web-socket/web-socket-client-manager.ts";
+import { createPlayer, type CreatePlayerPayload } from "../player/player.ts";
 
 export function registerGameRoutes(api: Router, webSocketClientManager: WebSocketClientManager) {
   /**
@@ -29,17 +30,28 @@ export function registerGameRoutes(api: Router, webSocketClientManager: WebSocke
   });
 
   /**
-   * Import a games
+   * Import db state. Games and plalyers
    */
-  api.post("/import-games", async (context) => {
-    const payload = (await context.request.body.json()) as (CreateGamePayload & {
-      time: number;
-    })[];
+  api.post("/import-db", async (context) => {
+    const { players, games } = (await context.request.body.json()) as {
+      players: CreatePlayerPayload[];
+      games: (CreateGamePayload & {
+        time: number;
+      })[];
+    };
+
+    for (const player of players) {
+      try {
+        await createPlayer(player);
+      } catch (error) {
+        console.error("Failed to create player", player, error);
+      }
+    }
 
     const importedGames: Game[] = [];
     const skippedGames: Game[] = [];
 
-    for (const game of payload) {
+    for (const game of games) {
       const created = await importGame(game);
       if (!created) {
         skippedGames.push(game);
@@ -60,8 +72,8 @@ export function registerGameRoutes(api: Router, webSocketClientManager: WebSocke
   /**
    * Delete one game
    */
-  api.delete("/game", async (context) => {
-    // isAuthenticated, requireAuth("game", "delete") // Auth requirement is removed because auth is not working properly atm
+  api.delete("/game", isAuthenticated, async (context) => {
+    // requireAuth("game", "delete") // Auth requirement is removed because auth is not working properly atm
     const payload = (await context.request.body.json()) as DeleteGamePayload;
     if (!payload) {
       throw new Error("payload is required");
@@ -80,7 +92,8 @@ export function registerGameRoutes(api: Router, webSocketClientManager: WebSocke
   /**
    * Delete all games
    */
-  api.delete("/games", isAuthenticated, requireAuth("game", "delete"), async (context) => {
+  api.delete("/games", isAuthenticated, async (context) => {
+    // requireAuth("game", "delete") // Auth requirement is removed because auth is not working properly atm
     const deleted = await deleteAllGames();
     context.response.body = deleted;
   });
