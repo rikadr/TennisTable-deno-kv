43bbbf9887f8eb5460d2a33c6dbc325f747bb9a6
Rikard Dotzler <rikardnoer@gmail.com>
2024-11-18 20:07:48 +0100

    restructure player page for larger screens

--- frontend/src/pages/player/player-page.tsx
+++ frontend/src/pages/player/player-page.tsx
@@ -26,91 +26,108 @@ export const PlayerPage: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <ProfilePicture name={name} clickToEdit border={8} shape="rounded" />
-      <div className="w-96 my-4">{summary && <PodiumPlace size="default" place={summary.rank} player={summary} />}</div>
-
-      {summary && (
-        <LineChart
-          width={Math.min(1000, width)}
-          height={300}
-          data={summary?.games}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="1 4" vertical={false} stroke="#FFFFFF" opacity={0.3} />
-          <YAxis
-            type="number"
-            domain={["dataMin", "dataMax"]}
-            tickFormatter={(value) => value.toLocaleString("no-NO", { maximumFractionDigits: 0 })}
-            stroke="#FFFFFF"
-          />
-          <XAxis dataKey="name" stroke="#FFFFFF" />
-          <Tooltip
-            formatter={(value) => [value.toLocaleString("no-NO", { maximumFractionDigits: 0 }), "Elo"]}
-            wrapperClassName="rounded-lg"
-            animationDuration={0}
-            content={<CustomTooltip />}
-          />
-          {["stroke", "main"].map((type) => {
-            return (
-              <Line
-                key={type}
-                type="monotone"
-                dataKey="eloAfterGame"
-                stroke={type === "main" ? "white" : stringToColor(name)}
-                dot={false}
-                animationDuration={100}
-                strokeWidth={type === "main" ? 0.5 : 5}
-              />
-            );
-          })}
-
-          <ReferenceLine
-            y={1000}
-            stroke="white"
-            label={{ value: "1 000", position: "insideBottom", fill: "white" }}
-            color="#"
-          />
-        </LineChart>
-      )}
-      {summary?.streaks && (
-        <>
-          <div className="">Longest win-streak 🔥🏆 {summary.streaks.longestWin}</div>
-          <div className="">Longest lose-streak 🔥💔 {summary.streaks.longestLose}</div>
-        </>
-      )}
-      <h1 className="text-2xl text-center mt-4">Points distribution</h1>
-      <div className="w-full max-w-2xl">
-        <PlayerPointsDistrubution name={summary?.name} />
-      </div>
-      <h1 className="text-2xl text-center mt-4">Games distribution</h1>
-      <div className="w-full max-w-2xl">
-        <PlayerGamesDistrubution name={summary?.name} />
-      </div>
-
-      <h1 className="text-2xl text-center mt-4">Last 10 games</h1>
-      <div className="flex flex-col divide-y divide-primary-text/50">
-        <div className="flex gap-4 text-base text-center mb-2">
-          <div className="w-32 text-left pl-2">Game</div>
-          <div className="w-12 pl-4 whitespace-nowrap">Elo +-</div>
-          <div className="w-32 text-right">Time</div>
+      <div className="flex flex-col items-center md:flex-row md:items-start ">
+        <div className="w-64">
+          <ProfilePicture name={name} clickToEdit border={8} shape="rounded" />
+          <div className="w-64 my-4">
+            {summary && <PodiumPlace size="default" place={summary.rank} player={summary} />}
+          </div>
         </div>
-        {reverseGames?.map((game, index) => (
-          <Link
-            key={(summary?.name ?? "-") + index + game.oponent}
-            to={`/player/${game.oponent}`}
-            className="bg-primary-background hover:bg-secondary-background/30 py-1 px-2 flex gap-4 text-xl font-light"
-          >
-            <div className="w-32 font-normal whitespace-nowrap">
-              {game.result === "win" ? "🏆 " : "💔 "} {game.oponent}
-            </div>
-            <div className="w-12 text-right">
-              {game.pointsDiff.toLocaleString("no-NO", {
-                maximumFractionDigits: 0,
+
+        {summary && (
+          <div>
+            <LineChart
+              width={Math.min(1000, width < 768 ? width : width - 300)}
+              height={300}
+              data={summary?.games}
+              margin={{ top: 5, right: 25, left: 0 }}
+            >
+              <CartesianGrid strokeDasharray="1 4" vertical={false} stroke="#FFFFFF" opacity={0.3} />
+              <YAxis
+                type="number"
+                domain={["dataMin", "dataMax"]}
+                tickFormatter={(value) => value.toLocaleString("no-NO", { maximumFractionDigits: 0 })}
+                stroke="#FFFFFF"
+              />
+              <XAxis dataKey="name" stroke="#FFFFFF" />
+              <Tooltip
+                formatter={(value) => [value.toLocaleString("no-NO", { maximumFractionDigits: 0 }), "Elo"]}
+                wrapperClassName="rounded-lg"
+                animationDuration={0}
+                content={<CustomTooltip />}
+              />
+              {["stroke", "main"].map((type) => {
+                return (
+                  <Line
+                    key={type}
+                    type="monotone"
+                    dataKey="eloAfterGame"
+                    stroke={type === "main" ? "white" : stringToColor(name)}
+                    dot={false}
+                    animationDuration={100}
+                    strokeWidth={type === "main" ? 0.5 : 5}
+                  />
+                );
               })}
+
+              <ReferenceLine
+                y={1000}
+                stroke="white"
+                label={{ value: "1 000", position: "insideBottom", fill: "white" }}
+                color="#"
+              />
+            </LineChart>
+            <div className="m-auto w-fit -mt-2">
+              <div className="">Longest win-streak 🔥🏆 {summary.streaks?.longestWin}</div>
+              <div className="">Longest lose-streak 🔥💔 {summary.streaks?.longestLose}</div>
             </div>
-            <div className="w-32 text-right text-base">{timeAgo(new Date(game.time))}</div>
-          </Link>
-        ))}
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col md:flex-row justify-evenly items-center md:items-start w-full">
+        <div className="w-full max-w-2xl flex flex-col justify-center">
+          <div className="flex flex-col items-center">
+            <h1 className="text-2xl text-center mt-4">Points distribution</h1>
+            <div className="w-full max-w-2xl">
+              <PlayerPointsDistrubution name={summary?.name} />
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <h1 className="text-2xl text-center mt-4">Games distribution</h1>
+            <div className="w-full max-w-2xl">
+              <PlayerGamesDistrubution name={summary?.name} />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h1 className="text-2xl text-center mt-4">Last 10 games</h1>
+          <div className="flex flex-col divide-y divide-primary-text/50">
+            <div className="flex gap-4 text-base text-center mb-2">
+              <div className="w-32 text-left pl-2">Game</div>
+              <div className="w-12 pl-4 whitespace-nowrap">Elo +-</div>
+              <div className="w-32 text-right">Time</div>
+            </div>
+            {reverseGames?.map((game, index) => (
+              <Link
+                key={(summary?.name ?? "-") + index + game.oponent}
+                to={`/player/${game.oponent}`}
+                className="bg-primary-background hover:bg-secondary-background/30 py-1 px-2 flex gap-4 text-xl font-light"
+              >
+                <div className="w-32 font-normal whitespace-nowrap">
+                  {game.result === "win" ? "🏆 " : "💔 "} {game.oponent}
+                </div>
+                <div className="w-12 text-right">
+                  {game.pointsDiff.toLocaleString("no-NO", {
+                    maximumFractionDigits: 0,
+                  })}
+                </div>
+                <div className="w-32 text-right text-base">{timeAgo(new Date(game.time))}</div>
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
