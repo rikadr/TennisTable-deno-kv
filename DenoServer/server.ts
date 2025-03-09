f5598047325a7daa0203ed9adf89f74f76e34c5e
Rikard Dotzler <rikardnoer@gmail.com>
2025-03-09 14:38:09 +0100

    debug logs and reduce cache size

--- DenoServer/server.ts
+++ DenoServer/server.ts
@@ -26,7 +26,9 @@ export const webSocketClientManager = new WebSocketClientManager();
 /**
  * Clear cashe on start or redeploy. New deployment could have changes in the data structure, invalidating the cache
  */
+console.log("Clearing cache");
 await clientDBCacheManager.clearCache();
+console.log("Cache cleared");
 
 /**
  * Register routes
