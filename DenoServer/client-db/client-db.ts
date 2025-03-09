8822196619a9d4101d50973594f4b6a8b751250e
Rikard Dotzler <rikardnoer@gmail.com>
2025-03-09 14:47:07 +0100

    exclude profile picture from cache

--- DenoServer/client-db/client-db.ts
+++ DenoServer/client-db/client-db.ts
@@ -1,19 +1,8 @@
 import { getAllGames } from "../game/game.ts";
-import { getAllPlayers, getProfilePicture, DEFAULT_PROFILE_PHOTO } from "../player/player.ts";
+import { getAllPlayers } from "../player/player.ts";
 import { getAllSignedUp } from "../tournament/tournament.ts";
 
 export async function getClientDbData() {
   const [players, games, signedUp] = await Promise.all([getAllPlayers(), getAllGames(), getAllSignedUp()]);
-
-  // Not very efficient to get pictures one by on per player. Suggest bulk fetch and distribute in map in code
-  const playersWithPhotos = await Promise.all(
-    players.map(async (player) => ({ ...player, photo: (await getProfilePicture(player.name)) || undefined })),
-  );
-
-  return {
-    players: playersWithPhotos,
-    defaultProfilePhoto: DEFAULT_PROFILE_PHOTO,
-    games,
-    tournament: { signedUp },
-  };
+  return { players, games, tournament: { signedUp } };
 }
