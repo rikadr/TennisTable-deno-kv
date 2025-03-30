9cb8ab0efd5fc79a1fb0b3997e6f755cfbbc64ef
Rikard Dotzler <rikardnoer@gmail.com>
2025-03-30 10:14:59 +0200

    back in business

--- frontend/src/wrappers/event-db-context.tsx
+++ frontend/src/wrappers/event-db-context.tsx
@@ -3,6 +3,7 @@ import { httpClient } from "../common/http-client";
 import { createContext, useContext } from "react";
 import { TennisTable } from "../client/client-db/tennis-table";
 import { EventType } from "../client/client-db/event-store/event-types";
+import { getClientConfig } from "../client/client-config/get-client-config";
 
 export function useEventDb() {
   return useQuery<EventType[]>({
@@ -31,39 +32,34 @@ export const useEventDbContext = () => useContext(EventDbContext);
 export const EventDbWrapper: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  // const eventsQuery = useEventDb();
-  // const client = getClientConfig();
+  const eventsQuery = useEventDb();
+  const client = getClientConfig();
 
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <p>Currently doing maintenance. Please come back later. Should be back up during sunday</p>
-    </div>
-  );
-  // if (eventsQuery.isLoading) {
-  //   return (
-  //     <div className="w-full h-screen flex items-center justify-center">
-  //       <div className="animate-ping w-fit text-9xl pr-4">{client.favicon}</div>
-  //     </div>
-  //   );
-  // }
-  // if (eventsQuery.isError) {
-  //   return (
-  //     <div className="w-full h-screen flex items-center justify-center">
-  //       <section>
-  //         <p>An error occured when loading events. Please try again</p>
-  //         <p>Error message: {eventsQuery.error?.message}</p>
-  //       </section>
-  //     </div>
-  //   );
-  // }
-  // if (!eventsQuery.data) {
-  //   return (
-  //     <div className="w-full h-screen flex items-center justify-center">
-  //       <p>Unable to load events. Please try again</p>
-  //     </div>
-  //   );
-  // }
+  if (eventsQuery.isLoading) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <div className="animate-ping w-fit text-9xl pr-4">{client.favicon}</div>
+      </div>
+    );
+  }
+  if (eventsQuery.isError) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <section>
+          <p>An error occured when loading events. Please try again</p>
+          <p>Error message: {eventsQuery.error?.message}</p>
+        </section>
+      </div>
+    );
+  }
+  if (!eventsQuery.data) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <p>Unable to load events. Please try again</p>
+      </div>
+    );
+  }
 
-  // const tennistableClass = (window.tennisTable = new TennisTable({ events: eventsQuery.data }));
-  // return <EventDbContext.Provider value={tennistableClass}>{children}</EventDbContext.Provider>;
+  const tennistableClass = (window.tennisTable = new TennisTable({ events: eventsQuery.data }));
+  return <EventDbContext.Provider value={tennistableClass}>{children}</EventDbContext.Provider>;
 };
