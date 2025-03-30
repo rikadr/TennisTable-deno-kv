f788c193d2b523c7bec7047294949f0eb0c5593d
Rikard Dotzler <rikardnoer@gmail.com>
2025-03-30 10:41:22 +0200

    map optio names to ids

--- DenoServer/migrations/migrations.ts
+++ DenoServer/migrations/migrations.ts
@@ -4,6 +4,31 @@ type Migration = {
   up: () => Promise<void> | void;
 };
 
+// const optioPlayers = {
+//   Rasmus: "fwSr2gbKZ9",
+//   Simone: "KvYCQupTl9",
+//   Alexander: "zAZF4oc08J",
+//   Fooa: "2XQllk6sPY",
+//   Peder: "OTGhYtvi0M",
+//   Erling: "ZYZhPub2Eo",
+//   Oskar: "mBwrdSejAz",
+//   "Fredrik H": "GLjRK9Ri3j",
+//   Rikard: "ch3ZaIB2qF",
+//   Ole: "0BFRJ8XiDS",
+//   Marius: "nnUvT8lPTH",
+//   Gina: "UCEYqaBBcK",
+//   Gustas: "8whk3RdC5f",
+//   Daniele: "BnKaNft4TF",
+//   "Ole Anders": "Jf0jHQZ0Z7",
+//   Kevin: "g5siQrzbun",
+//   "James 007": "Yb1ITjXe9t",
+//   "Chakib Youcefi": "fQZwfnr6IL",
+//   Tor: "LYp7GYa12X",
+//   "Alejandro 🌮": "seVb5J8bSZ",
+//   Markus: "3im4v9uUzl",
+//   Yngve: "Yz7pwV6aCB",
+// } as const;
+
 // DO NOT MODIFY EXISTING MIGRATIONS
 // ADD NEW MIGRATIONS TO THE END OF THE ARRAY
 
--- frontend/src/client/client-config/clients/optio-client.tsx
+++ frontend/src/client/client-config/clients/optio-client.tsx
@@ -2,6 +2,32 @@ import { TournamentDB } from "../../client-db/types";
 import { ClientConfig, Theme } from "../get-client-config";
 import { GuestClient } from "./guest-client";
 
+/** Mapping of old name to new ids */
+export const optioPlayers = {
+  Rasmus: "fwSr2gbKZ9",
+  Simone: "KvYCQupTl9",
+  Alexander: "zAZF4oc08J",
+  Fooa: "2XQllk6sPY",
+  Peder: "OTGhYtvi0M",
+  Erling: "ZYZhPub2Eo",
+  Oskar: "mBwrdSejAz",
+  "Fredrik H": "GLjRK9Ri3j",
+  Rikard: "ch3ZaIB2qF",
+  Ole: "0BFRJ8XiDS",
+  Marius: "nnUvT8lPTH",
+  Gina: "UCEYqaBBcK",
+  Gustas: "8whk3RdC5f",
+  Daniele: "BnKaNft4TF",
+  "Ole Anders": "Jf0jHQZ0Z7",
+  Kevin: "g5siQrzbun",
+  "James 007": "Yb1ITjXe9t",
+  "Chakib Youcefi": "fQZwfnr6IL",
+  Tor: "LYp7GYa12X",
+  "Alejandro 🌮": "seVb5J8bSZ",
+  Markus: "3im4v9uUzl",
+  Yngve: "Yz7pwV6aCB",
+} as const;
+
 export class OptioClient implements ClientConfig {
   id = process.env.REACT_APP_CLIENT;
   name = "Optio";
@@ -17,31 +43,29 @@ export const optioChristmasTournament: TournamentDB = {
   id: "randomid37",
   name: "Optio Christmas Tournament 2024 🏓🎅🏻",
   description:
-    "The social happening of the year, and a long awaited feature!! Sign up with your player and join the tournament 🚀",
+    "The social happening of the year, and a long awaited feature!! The first ever tournament hosted by the TennisTable app. Sign up with your player and join the tournament 🚀",
   startDate: 1732613408196, // Nov 26 2024 10:30:08 GMT+0100
   groupPlay: false,
   signedUp: [],
-  skippedGames: [{ advancing: "gcCBTUmYYT", eliminated: "8FKiTn8ZvP", time: 1732709055829 }], // Marius advancing, Erling eliminated
-
-  // TODO: function to set playerOrder based on elo at the time. if some players are not ranked, order them last by theirs signup order
+  skippedGames: [{ advancing: optioPlayers.Marius, eliminated: optioPlayers.Erling, time: 1732709055829 }], // Marius advancing, Erling eliminated
   playerOrder: [
-    "Q5G5GQhUqC", // "Rasmus",
-    "8lk8924c0J", // "Simone",
-    "FgKA0ySdSE", // "Alexander",
-    "V8ou4kR4fO", // "Fooa",
-    "wjucZdJbcd", // "Peder",
-    "8FKiTn8ZvP", // "Erling",
-    "WyoMEi0Vcw", // "Oskar",
-    "FhimcFfxFu", // "Fredrik H",
-    "iapK9z7xKz", // "Rikard",
-    "OEq03Oi7of", // "Ole",
-    "gcCBTUmYYT", // "Marius",
-    "yDfroTVdNV", // "Gina",
-    "TDoP7Fvuqa", // "Gustas",
-    "C4i3ATB1xH", // "Daniele",
-    "pcVKJJPBfl", // "Ole Anders",
-    "5CzDnlhtAb", // "Kevin",
-    "FyZc8yzxnY", // "James 007",
-    "wopCk0VzJr", // "Chakib Youcefi",
+    optioPlayers.Rasmus,
+    optioPlayers.Simone,
+    optioPlayers.Alexander,
+    optioPlayers.Fooa,
+    optioPlayers.Peder,
+    optioPlayers.Erling,
+    optioPlayers.Oskar,
+    optioPlayers["Fredrik H"],
+    optioPlayers.Rikard,
+    optioPlayers.Ole,
+    optioPlayers.Marius,
+    optioPlayers.Gina,
+    optioPlayers.Gustas,
+    optioPlayers.Daniele,
+    optioPlayers["Ole Anders"],
+    optioPlayers.Kevin,
+    optioPlayers["James 007"],
+    optioPlayers["Chakib Youcefi"],
   ],
 };
