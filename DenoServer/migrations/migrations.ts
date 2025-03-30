afbc4fef304da0048fe1d8dfc0fd91214b9392ee
Rikard Dotzler <rikardnoer@gmail.com>
2025-03-30 11:53:49 +0200

    remove migration for skimore

--- DenoServer/migrations/migrations.ts
+++ DenoServer/migrations/migrations.ts
@@ -1,6 +1,3 @@
-import { kv } from "../db.ts";
-import { uploadProfilePicture } from "../player/player.ts";
-
 type Migration = {
   name: string;
   description?: string;
@@ -12,30 +9,10 @@ type Migration = {
 // READ MORE LIMITATIONS IN README.md
 
 export const migrations: Migration[] = [
-  {
-    name: "migrate-skimore-profile-pictures",
-    up: async () => {
-      const idss = ["JI2IoMDt6V", "PC6lk9OwY0"];
-      for (const id of idss) {
-        const res = await getProfilePictureOld(id);
-        if (res) {
-          await uploadProfilePicture(id, res);
-          await kv.delete(["profile-picture-id", id]);
-        }
-      }
-    },
-  },
+  // {
+  //   name: "example-migration",
+  //   up: async () => {
+  //     console.log("Running example migration");
+  //   },
+  // },
 ];
-
-async function getProfilePictureOld(name: string): Promise<string | null> {
-  if (!name) {
-    throw new Error("name is required");
-  }
-
-  const res = await kv.get(["profile-picture-id", name]);
-
-  if (res.value) {
-    return res.value as string;
-  }
-  return null;
-}
