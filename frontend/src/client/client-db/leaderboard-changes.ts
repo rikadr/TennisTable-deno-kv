4dbbc5790864c37a9c9e0639957be748783e986e
Rikard Dotzler <rikardnoer@gmail.com>
2025-06-28 18:16:24 +0200

    remove console log

--- frontend/src/client/client-db/leaderboard-changes.ts
+++ frontend/src/client/client-db/leaderboard-changes.ts
@@ -46,7 +46,6 @@ export class LeaderboardChanges {
         });
       });
     }
-    console.log(scoreMaps.length);
 
     scoreMaps.forEach(({ time, map }) => {
       const leaderboard = Array.from(map)
