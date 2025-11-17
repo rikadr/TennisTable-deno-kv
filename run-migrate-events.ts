14b67e49b901ebf0b30d479253e115d22b460611
Rikard Dotzler <rd@optioincentives.no>
2025-11-17 21:51:26 +0100

    delete events dump and migration script

--- run-migrate-events.ts
+++
@@ -1,7 +0,0 @@
-import { optioEvents } from "./events-dump-optio.ts";
-import { updateEventTimes } from "./migrate-event-times.ts";
-
-const updatedEvents = updateEventTimes(optioEvents);
-
-await Deno.writeTextFile("updated-events.txt", JSON.stringify(updatedEvents));
-console.log(`Successfully wrote ${updatedEvents.length} events to updated-events.txt`);
